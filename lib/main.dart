10f04bf599794eb956672296b581ce80d97d8bf7
ElifDK <elif.koc@sabanciuniv.edu>
2025-03-07 11:55:22 +0300

    Added Buttons

--- lib/main.dart
+++ lib/main.dart
@@ -17,10 +17,22 @@ class Home extends StatelessWidget {
         title: Text('Buttons and Images'),
           centerTitle: true,
       ),
-      body: Center(
-          child: Icon(Icons.add_a_photo,
-          size: 50,
-          color: Colors.green[400],)),
+      body: Column(
+        children: [
+          Center(
+              child: Icon(Icons.add_a_photo,
+              size: 50,
+              color: Colors.green[400],)),
+          IconButton(onPressed: (){},
+              icon: Icon(Icons.access_alarm),),
+          ElevatedButton(onPressed: (){},
+              child: Text("Click me")),
+          ElevatedButton.icon(onPressed: (){}, 
+              icon: Icon(Icons.email),
+            label: Text('Send Email'),
+            )
+        ],
+      ),
     );
   }
 }
